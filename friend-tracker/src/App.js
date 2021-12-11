828f86f520582b6b23ac502720264036265ec5cd
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-12-10 23:04:43 -0500

    Change Fragment to short syntax <></>

--- friend-tracker/src/App.js
+++ friend-tracker/src/App.js
@@ -6,13 +6,13 @@ import './App.css';
 
  export const App = () => {
   return (
-    <Fragment>
-      <h1>Friend Tracker</h1>
-        <div className="content-container">
-          <WelcomeMessage/>
-          <ProfileInfo/>
-        </div>
-    </Fragment>
+    <>
+    <h1>Friend Tracker</h1>
+      <div className="content-container">
+        <WelcomeMessage/>
+        <ProfileInfo/>
+      </div>
+    </>
   );
 } 
 
--- friend-tracker/src/ProfileInfo.js
+++ friend-tracker/src/ProfileInfo.js
@@ -4,26 +4,26 @@ import './ProfileInfo.css';
 
 export const ProfileInfo =() => { 
     return (
-        <Fragment>
-            <div className="profile-pic-container">
-                <div className="profile-pic-wrap">
-                    <img 
-                        className="profile-pic"
-                        src= {process.env.PUBLIC_URL + './Profile-pic.png'}
-                        alt= {`${myProfileData.name}is wearing red t-shirt`}/>
-                </div>
+        <>
+        <div className="profile-pic-container">
+            <div className="profile-pic-wrap">
+                <img 
+                    className="profile-pic"
+                    src= {process.env.PUBLIC_URL + './Profile-pic.png'}
+                    alt= {`${myProfileData.name}is wearing red t-shirt`}/>
             </div>
-            <h2 className="content-heading">My Profile </h2> 
-            <h3>Name</h3>
-            <p>{myProfileData.name}</p>
-            <h3>Age</h3>
-            <p>{myProfileData.age}</p>
-            <h3>Bio</h3>
-            <p>{myProfileData.bio}</p>
-            <h3>Birthday</h3>
-            <p>{myProfileData.birthday}</p>
-            <h3>Interests</h3>
-            <p>{myProfileData.interests.join(', ')}</p>
-        </Fragment>     
+        </div>
+        <h2 className="content-heading">My Profile </h2> 
+        <h3>Name</h3>
+        <p>{myProfileData.name}</p>
+        <h3>Age</h3>
+        <p>{myProfileData.age}</p>
+        <h3>Bio</h3>
+        <p>{myProfileData.bio}</p>
+        <h3>Birthday</h3>
+        <p>{myProfileData.birthday}</p>
+        <h3>Interests</h3>
+        <p>{myProfileData.interests.join(', ')}</p>
+        </>     
     )
 }
